f2641078cc965a1f24c89bc136f4fc81c35d5906
sato <ng.plus6655@gmail.com>
2020-07-24 15:15:12 +0900

    init

---
+++ first-app/pages/articles/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-card>
+    <v-data-table :headers="headers" :items="articles"></v-data-table>
+  </v-card>
+</template>
+
+<script>
+export default {
+  layout: 'default',
+  data() {
+    return {
+      search: '',
+      headers: [
+        {
+          text: 'ID',
+          align: 'start',
+          filterable: false,
+          value: 'id',
+        },
+        { text: 'Title', value: 'title' },
+        { text: 'Description', value: 'description' },
+      ],
+      articles: [
+        {
+          id: 1,
+          title: 'Test Article1',
+          description: 'Testing Article1',
+        },
+        {
+          id: 2,
+          title: 'Test Article2',
+          description: 'Testing Article2',
+        },
+        {
+          id: 3,
+          title: 'Test Article3',
+          description: 'Testing Article3',
+        },
+        {
+          id: 4,
+          title: 'Test Article4',
+          description: 'Testing Article4',
+        },
+        {
+          id: 5,
+          title: 'Test Article5',
+          description: 'Testing Article5',
+        },
+        {
+          id: 6,
+          title: 'Test Article6',
+          description: 'Testing Article6',
+        },
+        {
+          id: 7,
+          title: 'Test Article7',
+          description: 'Testing Article7',
+        },
+        {
+          id: 8,
+          title: 'Test Article8',
+          description: 'Testing Article8',
+        },
+      ],
+    }
+  },
+}
+</script>
---
+++ first-app/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="text-center">
+        <p>Hello. Welcome to HomePage!!!</p>
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
